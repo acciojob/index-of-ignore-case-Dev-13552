516505a1a68ea8c7a0edcd2787223695d942b705
Dev-13552 <devrathour110@gmail.com>
2025-08-04 20:27:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 function indexOfIgnoreCase(s1, s2) {
+	s1 = s1.toLowerCase()
+	s2 = s2.toLowerCase()
+	console.log(s2)
   // write your code here
+	for(let i = 0; i < s1.length - s2.length + 1; i++){
+		let substr = s1.substring(i, i + s2.length)
+		console.log(substr)
+		if(substr === s2){
+			return i;
+		}
+	}
+	return -1;
+	
 }
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
+
 alert(indexOfIgnoreCase(s1, s2));
